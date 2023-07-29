9654abf2c0541baf678c2737c35f3f8acf4b8b42
Raju Khattri <35406115+Kiyoponn@users.noreply.github.com>
2023-07-29 11:35:08 +0530

    feat: add middleware for ratelimit

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,41 +1,16 @@
 import { Data } from '@/lib/types'
 import { getPromt } from '@/lib/utils'
-import { Ratelimit } from '@upstash/ratelimit'
-import { Redis } from '@upstash/redis'
 import { CohereStream, StreamingTextResponse } from 'ai'
-import { NextResponse } from 'next/server'
 
-// For Running on Edge
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-  // Getting the request data
   const data: Data = await req.json()
 
-  // Destructuring the data object
-  const { prompt, mode, tone, creativity, characters, uid } = data
+  const { prompt, mode, tone, creativity, characters } = data
 
-  // Rate Limiter init
-  const ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(7, '2 m'),
-  })
-
-  const identifier = uid // Limited by userid
-
-  const { success } = await ratelimit.limit(identifier)
-
-  if (!success) {
-    return NextResponse.json(
-      { error: 'You are being Rate limited' },
-      { status: 429 }
-    )
-  }
-
-  // Getting the prompt based on type
   const promptText = getPromt(mode, tone, characters, prompt)
 
-  // Body for POST request
   const body = JSON.stringify({
     model: 'command-xlarge-nightly',
     prompt: promptText,
---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+	redis: Redis.fromEnv(),
+	limiter: Ratelimit.slidingWindow(0, "2 m"),
+});
+
+export default async function middleware(
+	request: NextRequest,
+	_: NextFetchEvent
+): Promise<Response | undefined> {
+	const ip = request.ip ?? "127.0.0.1";
+	const { success } = await ratelimit.limit(
+		ip
+	);
+	return success
+		? NextResponse.next()
+		: new Response(
+			JSON.stringify({ message: 'You are being Rate limited' }),
+			{ status: 429 }
+		);
+}
+
+export const config = {
+	matcher: "/api/completion",
+};
