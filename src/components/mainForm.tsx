953b07cb72d78ac6d1eb58f1082b1fb0b8e947f6
Raju Khattri <35406115+Kiyoponn@users.noreply.github.com>
2023-07-31 11:44:07 +0530

    refactor: update

--- src/components/mainForm.tsx
+++ src/components/mainForm.tsx
@@ -7,7 +7,6 @@ import { useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { useSpinDelay } from 'spin-delay'
-import { z } from 'zod'
 
 import { Separator } from './ui/seprator'
 import { fontSansCD } from '@/lib/fonts'
@@ -66,29 +65,9 @@ export default function MainForm() {
 		},
 		onResponse: (res) => {
 			if (res.status === 429) {
-				toast.custom(() => (
-					<div className='text-sm flex items-center gap-1.5 w-[400px] font-medium mx-auto bg-red-100 text-red-500 p-4 rounded-md'>
-						<div data-icon>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 20 20'
-								fill='currentColor'
-								height='20'
-								width='20'
-							>
-								<title>Error Icon</title>
-								<path
-									fill-rule='evenodd'
-									d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z'
-									clip-rule='evenodd'
-								/>
-							</svg>
-						</div>
-						<div data-title>
-							Oops! You have already reached your maximum limit.
-						</div>
-					</div>
-				))
+				toast.error(
+					'Oops! You have already reached your maximum limit. Please try agin later.'
+				)
 			}
 		},
 		onFinish: () => {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,23 @@
 import { Ratelimit } from '@upstash/ratelimit'
 import { Redis } from '@upstash/redis'
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 const ratelimit = new Ratelimit({
 	redis: Redis.fromEnv(),
-	limiter: Ratelimit.slidingWindow(0, '2 m')
+	limiter: Ratelimit.slidingWindow(5, '10 m')
 })
 
 export default async function middleware(
 	request: NextRequest,
-	_: NextFetchEvent
 ): Promise<Response | undefined> {
 	const ip = request.ip ?? '127.0.0.1'
 	const { success } = await ratelimit.limit(ip)
+
 	return success
 		? NextResponse.next()
 		: new Response(JSON.stringify({ message: 'You are being Rate limited' }), {
-				status: 429
-		  })
+			status: 429
+		})
 }
 
 export const config = {
